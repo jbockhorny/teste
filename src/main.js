3570cb48e83ffb3257da00b4babd14eed1e62f64
joanilza <jbockhorny@gmail.com>
2020-06-18 20:10:27 -0300

    correções do css

--- src/main.js
+++ src/main.js
@@ -1,71 +1,79 @@
 const carousel = document.getElementById('carousel')
 const sectionCard = document.getElementById('card')
 
-
 const url = 'https://sky-frontend.herokuapp.com/movies';
-getApi()
 
 function getApi() {
 
   fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    data.map(element => {
-      getCarouel(element.items)
+    .then(res => res.json())
+    .then(data => {
+      data.map(element => {
+        templateCarousel(element.items)
+      })
     })
-  })
   fetch(url)
-  .then(res => res.json())
-  .then(d => {
-    d.map(el => {
-      getCategories(el.movies)
+    .then(res => res.json())
+    .then(data => {
+      getCategories(data[2].movies)
     })
-  })
 }
 
 
-function getCarouel(e) {
+function templateCarousel(item) {
   carousel.innerHTML =
     `<div class="carousel-item active">
-      <img class="d-block w-100" src="${e[0].images[0].url}"  alt="Poster do Filme ${e[0].title}">
+      <img class="d-block w-100" src="${item[0].images[0].url}"  alt="Poster do Filme ${item[0].title}">
       <div class="carousel-caption d-none d-md-block"></div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="${e[1].images[0].url}"  alt="Poster do Filme ${e[1].title}">
+      <img class="d-block w-100" src="${item[1].images[0].url}"  alt="Poster do Filme ${item[1].title}">
       <div class="carousel-caption d-none d-md-block"></div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="${e[2].images[0].url}"  alt="Poster do Filme ${e[2].title}">
+      <img class="d-block w-100" src="${item[2].images[0].url}"  alt="Poster do Filme ${item[2].title}">
       <div class="carousel-caption d-none d-md-block"></div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="${e[3].images[0].url}"  alt="Poster do Filme ${e[3].title}">
+      <img class="d-block w-100" src="${item[3].images[0].url}"  alt="Poster do Filme ${item[3].title}">
       <div class="carousel-caption d-none d-md-block"></div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="${e[4].images[0].url}"  alt="Poster do Filme ${e[4].title}">
+      <img class="d-block w-100" src="${item[4].images[0].url}"  alt="Poster do Filme ${item[4].title}">
       <div class="carousel-caption d-none d-md-block"></div>
     </div>`
 
 }
 
-function getCategories(e) {
-  // console.log(e.categories)
-  templateCategories(e)
-}
-
-function templateCategories(e) {
-  sectionCard.innerHTML = `<p>Ação e Aventura<p>`
-  for (let elem in e) {
-    for (let pudim in e[elem].images) {
-
-      if (e[elem].categories.indexOf("Ação e Aventura") >= 0)
-        sectionCard.innerHTML +=
-
-          `<div class="card" style="margin: 10px">
-    <img class="card-img-top" style="max-width: 18rem;" src="${e[elem].images[pudim].url}" alt="Card image cap">
-   </div>`
-      break;
-    }
+function getCategories(objCategories) {
+  let categories = new Map();
+  for (let i = 0; i < objCategories.length; i++) {
+    let splits = objCategories[i].categories.split(', ');
+    console.log(splits)
+    splits.forEach(element => {
+      let cardsCategories = categories.get(element);
+      if (cardsCategories === undefined) {
+        cardsCategories = [objCategories[i]];
+        categories.set(element, cardsCategories);
+      } else {
+        cardsCategories.push(objCategories[i])
+      }
+    });
   }
+
+  categories.forEach(function (objCategories, categoria) {
+    let titleCategoria = document.createElement('h5')
+    sectionCard.appendChild(titleCategoria)
+    let divCategoria = document.createElement('div')
+    sectionCard.appendChild(divCategoria);
+    divCategoria.classList.add('div-categoria')
+    titleCategoria.innerHTML += categoria
+    for (let i = 0; i < objCategories.length; i++) {
+      let menuCard = document.createElement('div');
+      divCategoria.appendChild(menuCard);
+      menuCard.classList.add('menuCard')
+      menuCard.innerHTML += `<img class="cardCategories" src="${objCategories[i].images[0].url}" alt="Card do filme">`
+    }
+  }, categories)
 }
+getApi()
